2bc524c1df59934cfe9cdf9725dd9ce69afd75be
D-Pankey <dvpankey32@aol.com>
2017-08-23 10:47:31 -0500

    this version has a list view with markers showing on the map

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-
 /*=======MODEL========*/
+// var locations;
 var mrMap;
 var markers = [];
 // The following function to initializes the map
@@ -13,8 +13,33 @@ function initMap() {
   });
 
 
-  //These are the markers that will be shown to the users
-  var locations = [{
+var infowindow = new google.maps.InfoWindow();
+
+
+// var marker, i;
+
+  // The following group uses the location array to create an array of markers on initialize.
+  for (var i = 0; i < locations.length; i++) {
+  var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(locations[i].location),
+    map: mrMap,
+    animation: google.maps.Animation.DROP,
+    icon: 'images/rail.png'
+ });
+   
+   
+
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent('<div><strong>' + locations[i].title+'</strong><br>' + locations[i].address + '<br>' +'Subway:' + locations[i].subway +'</div>');
+          infowindow.open(map, marker);
+        };
+      })(marker, i));
+
+  }
+}
+ var locations =[
+    {
       title: 'Brooklyn Bridge',
       location: {
         lat: 40.7713024,
@@ -61,85 +86,18 @@ function initMap() {
         lng: -73.9739882
       },
       address:'Central Park West at 79th Street NY,NY 10024',
-      subway:'A, B, C'
-    }
-
-
-  ];
-
-
-/*============View Model==========*/
-var infowindow = new google.maps.InfoWindow();
-
-
-// var marker, i;
-
-  // The following group uses the location array to create an array of markers on initialize.
-  for (var i = 0; i < locations.length; i++) {
-    marker = new google.maps.Marker({
-    position: new google.maps.LatLng(locations[i].location),
-    map: mrMap,
-    animation: google.maps.Animation.DROP,
-    icon: 'images/rail.png'
- });
-    //Extend the boundaries of map for wach marker
-   
-
-    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          infowindow.setContent('<div><strong>' + locations[i].title+'</strong><br>' + locations[i].address + '<br>' +'Subway:' + locations[i].subway +'</div>');
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-
-   //this puts the info content into each marker refer to
-
-
-
-
-
-
-    // Create a marker per location, and put into markers array.
-    // var marker = new google.maps.Marker({
-    //   map: mrMap,
-    //   position: position,
-    //   title: title,
-    //   animation: google.maps.Animation.DROP,
-    //   icon: 'images/rail.png',
-    //   id: i,
-    // });
+      subway:'A, B, C',
+    }];
   
-   //Push the marker to our array of markers.
+  this.filter = ko.observable("")
 
-   markers.push(marker);
+  var viewModel = function() {
+  //These are the markers that will be shown to the users
+  var self = this;
 
-
-
-
-
-
-
-   // When you click on each marker and infoWindow will open
-    
-    
-  //this function is going to tell the infowidow to open at that marker 
-  //and show info specific to that marker.  Each times a marker is clicked function is called
-  //and it sets content of infor window
-
-
-
-  }
 }
 
+   
 
-// var brooklynBridge = {
-//     lat: 40.706086,
-//     lng: -73.996864
-// // }
-// var image = 'images/rail.png'
-//     var markers = new google.maps.Marker({
-//         position: { lat: 40.706086, lng: -73.996864},
-//         map: mrMap,
-//         title: 'Hello Brooklyn! Brooklyn Bridge, New York, NY 10038',
-//         icon: 'images/rail.png'
-//     });
+
+   ko.applyBindings(new viewModel());
