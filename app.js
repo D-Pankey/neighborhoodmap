7f61bf69f57d15ea3497053cb3f846157f016f68
D-Pankey <dvpankey32@aol.com>
2017-08-25 11:25:09 -0500

    This map was working markers, clicking on the list populates the infowindow.  A third party API was also added

--- app.js
+++ app.js
@@ -1,96 +1,126 @@
+//========MODEL=======
+
+var map;
+
+//Locations that automatically show on map 
 var locations =[
     {
       title: 'Brooklyn Bridge',
       location: {
         lat: 40.7713024,
         lng: -73.9632393
-      },
-      address:'Bridge St, Brooklyn, NY 11201',
-      subway:'4,5,6'
-
-    },
+      } },
 
     {
       title: 'Central Park',
       location: {
         lat: 40.785091,
         lng: -73.968285
-      },
-      address:'59th Street and 5th Ave, NY, NY 10020',
-      subway: 'A, B, C, D, 1'
-
-    },
+      } },
 
     {
       title: 'Madison Square Garden',
       location: {
         lat: 40.750298,
         lng: -73.993324
-      },
-      address: '4 Pennsylvania Plaza, NY, NY ',
-      subway: 'A, C, E, 1, 2, 3'
-    },
+      }},
 
     {
       title: 'Broadway Theater',
       location: {
         lat: 40.753496986,
         lng: -73.985162726
-      },
-      address:'1681 Broadway NY, NY 10019',
-      subway:'B, D, E'
-
-    },
+      } },
 
     {
       title: 'The American Museum of Natural History ',
       location: {
         lat: 40.7813241,
         lng: -73.9739882
-      },
-      address:'Central Park West at 79th Street NY,NY 10024',
-      subway:'A, B, C'
-
-    }];
+      }}];
 
 
+function maperror() {
+    alert("There is a problem with Google Maps. Please reload page");
+}
+//Get Map to show up on page
 function initMap() {
-  mrMap = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     center: {
       lat: 40.7413549,
       lng: -73.99802439999996
     },
     zoom: 12
   });
- 
+
+
+ // An Info Window is created 
  var infowindow = new google.maps.InfoWindow();
 
  
+
+ // The following group uses the location array to create an array of markers on initialize.
   for (var i = 0; i < locations.length; i++) {
     // Get the position from the location array.
-    var position = locations[i].location;
+    this.position = locations[i].location;
     var title = locations[i].title;
     // Create a marker per location, and put into markers array.
     var marker = new google.maps.Marker({
       position: position,
       title: title,
-      map: mrMap,
+      map: map,
       animation: google.maps.Animation.DROP,
       id: i,
       icon: 'images/rail.png'
     });
+   
     locations[i].marker = marker;
     
+
+    // This function populates the infowindow when the marker is clicked. 
      google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
-          infowindow.setContent('<div><strong>' + locations[i].title+'</strong><br>' + locations[i].address + '<br>' +'Subway:' + locations[i].subway +'</div>');
+    //foursquare api variables
+var client_id = 'WNHYGVIHUYC25AUSWKYUP0EMCC4OSNTQMR5UYOJ51THYFXAB';
+var client_secret = '1ZBNGSRKILX2G5T5VVHWOE1YSWVLUOPFKHX11UH5OQWMLKFO';
+
+
+var fourSquareUrl = 'https://api.foursquare.com/v2/venues/search?ll=' + self.position.lat + ',' + self.position.lng + '&query=' + marker.title+'&client_id=WNHYGVIHUYC25AUSWKYUP0EMCC4OSNTQMR5UYOJ51THYFXAB&client_secret=1ZBNGSRKILX2G5T5VVHWOE1YSWVLUOPFKHX11UH5OQWMLKFO&v=20170824';
+//Get JSON request to get data from FourSquare
+
+
+
+        $.getJSON(fourSquareUrl).done(function(data) {
+            console.log(data);
+        var results = data.response.venues["0"];
+        //Get title from foursquare
+       
+        this.zip=results.location.postalCode;
+        this.address=results.location.formattedAddress;
+        // console.log(this.zip);
+          infowindow.setContent('<div><strong>' + locations[i].title +'<div>'+this.address+'</div>'+'</strong></div>');
+
+    }).fail(function() {
+        console.log(self.position);
+        alert("There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.");
+    });
+        
+
+
           infowindow.open(map, marker);
+          // sets animation to bounce 2 times when marker is clicked
+                    marker.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(function() {
+                        marker.setAnimation(null);
+                    }, 2130);
+
         };
       })(marker, i));
 
+ 
   };
 
- 
+
  
  ko.applyBindings(new ViewModel())
 };
@@ -99,11 +129,14 @@ var Loc = function(data) {
   this.title = data.title;
   this.location = data.location;
   this.marker = data.marker;
+  
 };
 
 var ViewModel = function() {
   var self = this;
 
+
+//filter the location
   self.listLoc = ko.observableArray();
 
   locations.forEach(function(locItem) {
@@ -121,15 +154,23 @@ var ViewModel = function() {
       return self.listLoc();
     } else {
       return ko.utils.arrayFilter(self.listLoc(), function(item) {
-        // set all markers visible (false)
+    
+     // set all markers visible (false)
         var result = (item.title.toLowerCase().search(filter) >= 0)
         item.marker.setVisible(result);
         return result;
       });
     }
   });
-
+//Clicking a location on the list animates its associated map marker
   self.setLoc = function(clickedLoc) {
+    console.log(clickedLoc)
+//triggers Infowindow to open when item on list is clicked
+    google.maps.event.trigger(clickedLoc.marker, 'click')
     clickedLoc.marker.setAnimation(google.maps.Animation.BOUNCE);
+         setTimeout(function() {
+        clickedLoc.marker.setAnimation(null);
+      }, 2000);
   };
+ 
 }
